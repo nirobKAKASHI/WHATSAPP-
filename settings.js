3db4e5e002d6670375aabe43b6dd5f48f579e597
benz13110 <bensonbichage733@gmail.com>
2025-02-06 15:35:45 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
+  botName: "benz bot",
   botOwner: 'Professor', // Your name
   ownerNumber: '917023951514', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
