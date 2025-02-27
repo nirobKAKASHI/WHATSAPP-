8fe5a9084a00c3d58937dc3eef1beda11d497be6
kimcoder12 <iankimathi90@gmail.com>
2025-02-27 12:12:21 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "&***&",
+  botOwner: 'kimcoder12, // Your name
+  ownerNumber: '254700185883', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
