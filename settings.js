8ea44aac9ce84475f107d2566f07380a12311199
0721932901G <160822951+0721932901G@users.noreply.github.com>
2025-03-03 08:07:19 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Generali',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Generali",
+  botOwner: 'Generali', // Your name
+  ownerNumber: '254721932901', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
