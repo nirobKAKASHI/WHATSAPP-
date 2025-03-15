27e8f0351740d390f33b6e8028b26fd0467f90a7
Samirsamai <115250443+Samirsamai@users.noreply.github.com>
2025-03-15 18:02:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "davil Bot",
+  botOwner: 'samir samai', // Your name
+  ownerNumber: '918944040713', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
