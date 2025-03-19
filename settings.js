d3b9e65c77f3bb1bdba4a0a3d1fd40bec80b48a1
Divinekeyz123 <christopherdivine84@gmail.com>
2025-03-19 13:41:24 +0100

    Update settings.js
    Changed to public

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Divinekeyz bot",
+  botOwner: 'Divinekeyz', // Your name
+  ownerNumber: '2347038527443', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
