bdbdc1cfd376ff45cbf1479645fac4e0abac8b00
Breedrmd <kidnairobian94@gmail.com>
2025-04-02 12:14:02 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Breedermd',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "𝐌𝐁𝐔𝐙𝐈",
+  botOwner: '𝐌𝐁𝐔𝐙𝐈-𝐅𝐋𝐘𝐍𝐍', // Your name
+  ownerNumber: '254782944849', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
