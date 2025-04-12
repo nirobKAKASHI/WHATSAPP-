2012a5c18acd181603d425e7219e05a3a8410f31
Knightbotx <vincentobi191@gmail.com>
2025-04-12 10:39:42 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'Professor', //VINI Your name
+  ownerNumber: '917023951514', //+2349162520865Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
