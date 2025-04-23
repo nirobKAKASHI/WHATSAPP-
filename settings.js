d3fc5901ffe86cbdce7125dc4e1ce07178cb5fd8
Hackerboy68 <163637208+Hakerboy68@users.noreply.github.com>
2025-04-23 22:09:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'kaminari',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "kaminari bot",
+  botOwner: 'Atharva', // Your name
+  ownerNumber: '919881459658', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
