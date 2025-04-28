df96c2daa87064c2235c00f9b439e2e1316d8dd0
Quanta-spec <alhassanabdulaziz330@gmail.com>
2025-04-28 13:56:04 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', //VINI Your name
-  ownerNumber: '917023951514', //+2349162520865Your number
+  botOwner: 'Abdul Aziz', //VINI Your name
+  ownerNumber: '233594974209', //+2349162520865Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
