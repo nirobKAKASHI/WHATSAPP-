1548c7a6503651e45268434012914197fa8055db
guts-md <91badboy99@gmail.com>
2025-04-29 08:23:11 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Killer",
+  botName: "Gutso",
   botOwner: 'Guts|Ahmed', // Your name
   ownerNumber: '201508220480', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "( إِنَّ اللَّهَ وَمَلَائِكَتَهُ يُصَلُّونَ عَلَى النَّبِيِّ ۚ يَا أَيُّهَا الَّذِينَ آمَنُوا صَلُّوا عَلَيْهِ وَسَلِّمُوا تَسْلِيمًا )",
   version: "1.0.0",
 };
 
