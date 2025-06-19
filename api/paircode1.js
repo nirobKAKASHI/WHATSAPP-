18e7ab3a3a0524f74cec245b0e42dd19f20820c4
Toxicant1 <isaac0maina@gmail.com>
2025-06-20 01:59:55 +0300

    Create paircode1.js

---
+++ api/paircode1.js
@@ -0,0 +1,11 @@
+export default function handler(req, res) {
+  res.status(200).send(`
+    <html>
+      <head><title>Pair Code 1</title></head>
+      <body style="text-align:center;font-family:sans-serif;">
+        <h2>🔐 Pair Code 1</h2>
+        <p>Coming soon... BeltahBot pairing system will display code here.</p>
+      </body>
+    </html>
+  `);
+}
