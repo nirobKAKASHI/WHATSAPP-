8ee2308ae388c08de84daebbc0e0db08503b98d4
Junior20111106 <boyboy20111106@gmail.com>
2025-07-21 13:48:47 +0200

    Update settings.js
    Idk what u want I'm just a boy

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
-const settings = {
-  packname: 'justice by junior ',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'justice.01', 
-  ownerNumber: '+27675859928 ', 
-  giphyApiKey: 'nexus~oQR1kSRT#iYiJ-HQs2XSbt6yOUv53yBsKuIK2YfqYytLclaPYl-w',
-  commandMode "public",
-  description: "This is a bot for managing group commands and automating tasks.",
-  version: "2.0.6",
-};
-
-module.exports = settings;
+1  const settings = {
+2    packname: 'Justice by Junior',
+3    author: '‎', // Optional: add your name
+4    botName: "Knight Bot",
+5    botOwner: 'justice.01',
+6    ownerNumber: '+27675859928',
+7    nexusApiKey: 'nexus~oQR1kSRT#iYiJ-HQs2XSbt6yOUv53yBsKuIK2YfqYytLclaPYl-w',
+8    commandMode: "public",
+9    description: "This is a bot for managing group commands and automating tasks.",
+10   version: "2.0.6",
+11 };
+12 
+13 module.exports = settings;
