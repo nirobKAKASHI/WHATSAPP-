26ff1145538e35d13040a56404fd8a5e3c263e4e
Junior20111106 <boyboy20111106@gmail.com>
2025-07-21 13:43:19 +0200

    Update settings.js
    ジュニアによるジャスティス

--- settings.js
+++ settings.js
@@ -2,10 +2,10 @@ const settings = {
   packname: 'justice by junior ',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // justice by junior 
-  ownerNumber: '+27 675859928 ', //Set your number here without + symbol, just add country code & number without any space
-  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  botOwner: 'justice.01', 
+  ownerNumber: '+27675859928 ', 
+  giphyApiKey: 'nexus~oQR1kSRT#iYiJ-HQs2XSbt6yOUv53yBsKuIK2YfqYytLclaPYl-w',
+  commandMode "public",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.6",
 };
