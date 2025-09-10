be61d6e4c3d106cc2a37e5449c16bb1162d9ce4b
ＮＩＲＯＢ ᶻ 𝗓 𐰁 <yung0zhen@gmail.com>
2025-09-10 23:34:24 +0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "「 𝐍𝐈𝐑𝐎𝐁 」(∩˃o˂∩)♡",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '8801772594397', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
